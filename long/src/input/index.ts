65c90d37d9a0bea51d162329d602e24b7d8db421
ej-shafran <ejshafran@hilma.tech>
2023-05-07 20:25:40 +0300

    input part 1

--- long/src/input/index.ts
+++ long/src/input/index.ts
@@ -1,4 +1,59 @@
+import chalk from "chalk";
 import { ExtractedResults } from "../extract";
+import inquirer from "inquirer";
 
 export async function input(results: ExtractedResults) {
+  const answers = await inquirer.prompt([
+    {
+      type: "confirm",
+      message: `Is ${chalk.blue(results.name)} the correct ${chalk.yellow(
+        "collection name"
+      )}?`,
+      name: "name",
+    },
+    {
+      type: "confirm",
+      message: `Is ${chalk.blue(results.org)} the correct ${chalk.yellow(
+        "orginization name"
+      )}?`,
+      name: "org",
+    },
+    {
+      type: "confirm",
+      message: `Is ${chalk.blue(
+        results.description
+      )} the correct ${chalk.yellow("description")}?`,
+      name: "description",
+    },
+    {
+      type: "confirm",
+      message: `Is ${chalk.blue(results.updateRate)} the correct ${chalk.yellow(
+        "update rate"
+      )}?`,
+      name: "updateRate",
+    },
+    {
+      type: "confirm",
+      message: `Is ${chalk.blue(
+        results.updateMethod
+      )} the correct ${chalk.yellow("update method")}?`,
+      name: "updateMethod",
+    },
+    {
+      type: "confirm",
+      message: `Is "${chalk.blue(results.formats)}" the correct ${chalk.yellow(
+        "list of resource formats"
+      )}?`,
+      name: "formats",
+    },
+    {
+      type: "confirm",
+      message: `Is ${chalk.blue(
+        results.hasAPI
+      )} the correct answer to "Has API?"`,
+      name: "hasAPI",
+    },
+  ]);
+
+  console.log(answers);
 }
--- long/src/longscrape.ts
+++ long/src/longscrape.ts
@@ -1,6 +1,7 @@
 import chalk from "chalk";
 import puppeteer from "puppeteer";
 import { extract } from "./extract";
+import { input } from "./input";
 
 export async function longFetch(url: string) {
   console.log(`Loading new ${chalk.green("puppeteer")} instance...`);
@@ -20,10 +21,12 @@ export async function longFetch(url: string) {
   await page.goto(url, { waitUntil: "networkidle0" });
 
   console.log(`Extracting all easily readable data from ${chalk.blue(url)}...`);
+  console.log();
   const data = await extract(browser, page);
 
   // start prompting here!
-  console.log(data);
+  console.log();
+  await input(data);
 
   return process.exit(0);
 }
