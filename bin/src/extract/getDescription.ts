e04ca3570778008cef3c1bc2fd64e940fa66f36d
ej-shafran <ejshafran@hilma.tech>
2023-05-08 11:18:45 +0300

    Fix issue with description

--- bin/src/extract/getDescription.ts
+++ bin/src/extract/getDescription.ts
@@ -4,15 +4,14 @@ import { Page } from "puppeteer";
 export async function getDescription(page: Page) {
   console.log(chalk.bold("Getting description..."));
 
-  const desc = await page.$eval(
-    ".notes.embedded-content",
-    (el) => el.textContent
-  );
+  try {
+    const desc = await page.$eval(
+      ".notes.embedded-content",
+      (el) => el.textContent
+    );
 
-  if (!desc) {
-    console.error("Could not find `.notes.embedded-content`.");
-    return process.exit(1);
+    return desc?.trim() ?? "";
+  } catch (error) {
+    return "";
   }
-
-  return desc.trim();
 }
